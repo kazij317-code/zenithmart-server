9165d3bf15909f2ead1c3b6939afd2d1a5296d78
ZenithMart Developer <dev@zenithmart.com>
2026-07-18 01:40:00 +0600

    feat: establish connection to MongoDB database

--- index.ts
+++ index.ts
@@ -4,9 +4,12 @@ dns.setServers(["1.1.1.1", "1.0.0.1"]);
 const express = require("express");
 const dotenv = require("dotenv");
 const cors = require("cors");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 
 dotenv.config();
 
+const uri = process.env.MONGO_DB_URI;
+const dbName = process.env.AUTH_DB_NAME || "zenithmart";
 const app = express();
 const PORT = process.env.PORT || 5000;
 const clientUrl = process.env.CLIENT_URL || "http://localhost:3000";
@@ -22,6 +25,32 @@ app.use(express.json());
 app.get("/", (req: any, res: any) => {
   res.send("Hello ZenithMart Server!");
 });
+
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+
+const db = client.db(dbName);
+const productsCollection = db.collection("products");
+const userCollection = db.collection("user");
+const ordersCollection = db.collection("orders");
+const cartCollection = db.collection("cart");
+const favoritesCollection = db.collection("favorites");
+
+// Connect to MongoDB
+async function connectDB() {
+  try {
+    await client.connect();
+    console.log("Connected to MongoDB successfully!");
+  } catch (error) {
+    console.error("Failed to connect to MongoDB", error);
+  }
+}
+connectDB();
 
 app.listen(PORT, () => {
   console.log(`ZenithMart Server is running on port ${PORT}`);
